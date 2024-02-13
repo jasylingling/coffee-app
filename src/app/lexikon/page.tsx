dc2a637bf660af996e5d47a0f0a9bb1dcfe79199
Jasmin Fischli <jasmin@smartive.ch>
2024-02-13 09:53:39 +0000

    feat(#31): add lexikon

--- src/app/lexikon/page.tsx
+++ src/app/lexikon/page.tsx
@@ -1,8 +1,49 @@
+'use client';
+
+import InfoCard from '@/components/cards/info-card';
 import Header from '@/components/header/header';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { APIENDPOINT_HOT, APIENDPOINT_COLD } from '@/lib/constants';
+import { Info } from '@/components/cards/info-card';
 
 const LexikonPage: FC = () => {
-  return <Header>Lexikon</Header>;
+  const [hotBeverages, setHotBeverages] = useState<Info[]>([]);
+  const [coldBeverages, setColdBeverages] = useState<Info[]>([]);
+
+  const getHotBeverages = async () => {
+    const response = await fetch(APIENDPOINT_HOT);
+    const data = await response.json();
+    setHotBeverages(data);
+  };
+
+  const getColdBeverages = async () => {
+    const response = await fetch(APIENDPOINT_COLD);
+    const data = await response.json();
+    setColdBeverages(data);
+  };
+
+  useEffect(() => {
+    getHotBeverages();
+    getColdBeverages();
+  }, []);
+
+  return (
+    <>
+      <Header>Lexikon</Header>
+      <div className="brews-wrapper mt-6 flex flex-wrap">
+        {[...hotBeverages]
+          .sort((a, b) => a.title.localeCompare(b.title)) // Sortierung
+          .map((recipe, id) => (
+            <InfoCard key={id} info={recipe} type="hot" badgeText="hot" contentToShow="description" />
+          ))}
+        {[...coldBeverages]
+          .sort((a, b) => a.title.localeCompare(b.title)) // Sortierung
+          .map((recipe, id) => (
+            <InfoCard key={id} info={recipe} type="cold" badgeText="cold" contentToShow="description" />
+          ))}
+      </div>
+    </>
+  );
 };
 
 export default LexikonPage;
